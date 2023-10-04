4104b68f70686146ada8720be4f0b505bd11f7df
Chanuka Samila <chanukasamila@gmail.com>
2023-10-04 14:59:09 +0530

    Added property class and iproperty interface.

---
+++ src/app/property/models/property.ts
@@ -0,0 +1,14 @@
+export class Property {
+    Id? : number;
+    Name : string;
+    Type : string;
+    Price : number; 
+
+    constructor(Id: number = 0, Name: string = '', Type: string = '', Price: number = 0)
+    {
+        this.Id = Id;
+        this.Name = Name;
+        this.Type = Type;
+        this.Price = Price;
+    }
+}
